b8985da04af36e77898fb5555ba2b06922135161
Mario Sinovcic <Mario.Sinovcic@myob.com>
2019-11-21 14:18:29 +1300

    Init For Master-Mind

---
+++ Kata-Master-Mind/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kata_Master_Mind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
